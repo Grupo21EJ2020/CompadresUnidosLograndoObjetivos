d566f7236ced5726e242370c60066004f4410191
Alejandro01598 <alejajosez7@gmail.com>
2020-05-26 23:08:00 -0500

    Agregue consulta de empleados

--- archivos/menu.py
+++ archivos/menu.py
@@ -24,7 +24,7 @@ if Menu == 1:
             nuevoEmpleado1.Agregar_Empleado()
             print ("----ELEGIR OTRA OPCION----\n" )
             Menu = int(input("1.Empleado \n2.Curso \n3.Tema \n4.Video \n:"))
-        if submenu == 2: 
+        if submenu == 3: 
             print ("-------DATOS DE EMPLEADOS------:")
             idEmpleado =""
             nombre =""
@@ -32,18 +32,10 @@ if Menu == 1:
             nuevoEmpleado1 = Empleado_agregar(idEmpleado, nombre, direccion)
             nuevoEmpleado1.Agregar_Empleado()
             print ("----ELEGIR OTRA OPCION----\n" )
-            Menu = int(input("1.Empleado \n2.Curso \n3.Tema \n4.Video \n:"))
-        if submenu == 2: 
-            print ("-------DATOS DE EMPLEADOS------:")
-            idEmpleado =""
-            nombre =""
-            direccion = ""
-            nuevoEmpleado1 = Empleado_agregar(idEmpleado, nombre, direccion)
-            nuevoEmpleado1.Agregar_Empleado()
-            print ("----ELEGIR OTRA OPCION----\n" )
-            Menu = int(input("1.Empleado \n2.Curso \n3.Tema \n4.Video \n:"))    
-        elif submenu == 4: 
-            print ("Se te mostrara la imnformacion de los empleados")
+            Menu = int(input("1.Empleado \n2.Curso \n3.Tema \n4.Video \n:"))   
+        elif submenu == 4:
+            print("---------Consulta de Empleados----------\n") 
+            print ("----Se te mostrara la informacion de los empleados--- \n")
             archivo = open("./archivos/empleados.txt",encoding="utf8")
             print(archivo.read())
             salir = int (input("---Quieres Continuar??---\n1. SI \n2. NO : "))
