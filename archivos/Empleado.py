d54954ff0094940f9947ce6cc82657a88e520176
Alejandro01598 <alejajosez7@gmail.com>
2020-05-27 19:09:21 -0500

    se agrego metodo borrar empleado funciona :v

--- archivos/Empleado.py
+++ archivos/Empleado.py
@@ -1,31 +1,51 @@
 class Empleado_agregar:
     def __init__(self,idEmpleado,Nombre,Direccion):
-        self.idEmpleado = idEmpleado
-        self.Nombre = Nombre
-        self.Direccion = Direccion 
+        self.__idEmpleado = idEmpleado
+        self.__Nombre = Nombre
+        self.__Direccion = Direccion 
+    
+
+    @property
+    def idEmpleado (self):
+        return self.__idEmpleado
+    
+    @idEmpleado.setter
+    def idEmpleado(self,valor):
+        self.__idEmpleado=valor
+   
+    @property
+    def Nombre (self):
+        return self.__Nombre
+    
+    @Nombre.setter
+    def Nombre(self,valor):
+        self.__Nombre=valor
+
+    @property
+    def Direccion (self):
+        return self.__Direccion
+    
+    @Direccion.setter
+    def Direccion(self,valor):
+        self.__Direccion=valor
+
 
     def Empleado(self):
         archivo = open("./archivos/empleados.txt","a",encoding="utf8")
-        idempleado = input("ID EMPLEADO: \n")
-        print("NOMBRE DEL EMPLEADO: ")
-        nombre = input("")
-        print("DIRECCION:")
-        direccion = input("")
-        archivo.write(idempleado + "|" + nombre + "|" + direccion + "\n")
+        archivo.write(self.__idEmpleado + "|" + self.__Nombre + "|" + self.__Direccion + "\n")
         archivo.close()
     
 
     def consultar_empleado(self):
-        archivo = open("./archivos/empleados.txt",encoding="utf8")
+        archivo = open("./archivos/empleados.txt","r",encoding="utf8")
         print(archivo.read())
 
     
     def eliminar_empleado(self):
-        archivo = open("./archivos/empleados.txt","a",encoding="utf8")
-        idempleado = input("ID EMPLEADO: \n")
-        print("NOMBRE DEL EMPLEADO: ")
-        nombre = input("")
-        print("DIRECCION:")
-        direccion = input("")
-        archivo.(idempleado,nombre,direccion)
-        archivo.close
+        f = open("./archivos/empleados.txt","r",encoding="utf8")
+        chain = f.read()    #guardamos su contenido en una variable
+        chain = chain.replace(self.__idEmpleado + "|" + self.__Nombre + "|" + self.__Direccion,"")    
+        f.close()   
+        otro = open("./archivos/empleados.txt","w",encoding="utf8")    
+        otro.write(chain)    
+        otro.close()   
--- archivos/menu.py
+++ archivos/menu.py
@@ -1,32 +1,33 @@
 from Empleado import Empleado_agregar
 Menu =  int(input("------MENU----\n1.Empleado \n2.Curso \n3.Tema \n4.Video \n: "))
-print ("Elige una Opcion")
+print ("---Elige una Opcion---")
 
 if Menu == 1: 
     while Menu >= 1:
         submenu = int(input("--------SUBMENU------ \n1.Agregar \n2.Borrar \n3.Modificar \n4.Consultar \n5. Ver Empleado Especifico \n:"))
         if submenu == 1: 
-            print ("-------DATOS DE EMPLEADOS------:")
-            idEmpleado =""
-            nombre =""
-            direccion = ""
-            nuevoEmpleado1 = Empleado_agregar(idEmpleado, nombre, direccion)
+            print ("-------DATOS DE EMPLEADOS------")
+            idEmpleado=input("Ingresa el ID del empleado\n")
+            Nombre =input("Ingresa el nombre del empleado\n")
+            Direccion = input("Ingresa la direecion del empleado\n1")
+            nuevoEmpleado1 = Empleado_agregar(idEmpleado, Nombre, Direccion)
             nuevoEmpleado1.Empleado()
             print ("----ELEGIR OTRA OPCION----\n" )
             Menu = int(input("1.Empleado \n2.Curso \n3.Tema \n4.Video \n:"))
 
         if submenu == 2: 
-            print ("-------BORRAR EMPLEADOS------: \n Ingresa el ID del empleado para borrar")
-            idEmpleado= ""
-            nombre= ""
-            direccion= ""
-            deleteEmpleado1 = Empleado_agregar(idEmpleado,nombre,direccion)
-            deleteEmpleado1.eliminar_empleado()
+            print ("-------BORRAR EMPLEADOS------\n")
+            idEmpleado=input("Ingresa el ID del empleado\n")
+            Nombre =input("Ingresa el nombre del empleado\n")
+            Direccion = input("Ingresa la direccion del empleado \n")
+            eliminarEmpleado1 = Empleado_agregar(idEmpleado,Nombre,Direccion)
+            eliminarEmpleado1.eliminar_empleado()
             print ("----ELEGIR OTRA OPCION----\n" )
             Menu = int(input("1.Empleado \n2.Curso \n3.Tema \n4.Video \n:"))
         if submenu == 3: 
             print ("-------D EMPLEADOS------:")
             idEmpleado =""
+
             nombre =""
             direccion = ""
             print ("----ELEGIR OTRA OPCION----\n" )
